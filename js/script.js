8f3796eada96b210a8c533f833078f615e678dc0
Alice Souza <alicepsouzaba1998@gmail.com>
2022-02-05 22:22:35 -0300

    barrier created with DOM manipulation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-const newElement = (tagName, className) => {
+function newElement (tagName, className) {
   const element = document.createElement(tagName);
   element.className = className;
   return element;
 };
+
+function Barrier (reverse = false) {
+  this.element = newElement('div', 'barrier');
+  const border = newElement('div', 'border');
+  const body = newElement('div', 'body');
+  this.element.appendChild(reverse ? body : border);
+  this.element.appendChild(reverse ? border : body);
+  this.setHeight = height => body.style.height = `${height}px`;
+};
+
+// const b = new Barrier(true);
+// b.setHeight(200);
+// document.querySelector('[wm-flappy]').appendChild(b.element)
+
